22501a4d375a954604451a1af00ac177604184da
t-ria <kristalin.shet@gmail.com>
2022-04-15 11:32:47 +0300

    классы игры и меню

---
+++ class_menu.py
@@ -0,0 +1,56 @@
+from telebot import types
+
+class Users:
+    activeUsers = {}
+
+    def __init__(self, chat_id, user_json):
+        self.id = user_json["id"]
+        self.isBot = user_json["is_bot"]
+        self.firstName = user_json["first_name"]
+        self.userName = user_json["username"]
+        self.languageCode = user_json["language_code"]
+
+        self.__class__.activeUsers[chat_id] = self
+
+    def __str__(self):
+        return f"Name user: {self.firstName}   id: {self.userName}   lang: {self.languageCode}"
+
+    def getUserHTML(self):
+        return f"Name user: {self.firstName}   id: <a href='https://t.me/{self.userName}'>{self.userName}</a>   lang: {self.languageCode}"
+
+
+    @classmethod
+    def getUser(cls, chat_id):
+        return cls.activeUsers.get(chat_id)
+# -----------------------------------------------------------------------
+class Menu:
+    hash = {}
+    cur_menu = None
+
+    def __init__(self, name, buttons=None, parent=None, action=None):
+        self.parent = parent
+        self.name = name
+        self.buttons = buttons
+        self.action = action
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=5)
+        markup.add(*buttons)
+        self.markup = markup
+
+        self.__class__.hash[name] = self  # в классе содержится словарь, со всеми экземплярами класса, обновим его
+
+    @classmethod
+    def getMenu(cls, name):
+        menu = cls.hash.get(name)
+        if menu != None:
+            cls.cur_menu = menu
+        return menu
+
+
+m_main = Menu("Главное меню", buttons=["Развлечения", "Игры", "Помощь(нужна мне)"])
+
+m_games = Menu("Игры", buttons=["Камень, ножницы, бумага", "Игра в 21", "Выход"], parent=m_main)
+m_game_21 = Menu("Игра в 21", buttons=["Карту!", "Стоп!", "Выход"], parent=m_games, action="game_21")
+m_game_rsp = Menu("Камень, ножницы, бумага", buttons=["Камень", "Ножницы", "Бумага", "Выход"], parent=m_games, action="game_rsp")
+
+m_fun = Menu("Развлечения", buttons=["Прислать собаку", "Прислать анекдот", "Выход"], parent=m_main)
--- аааааа.py
+++ аааааа.py
@@ -2,7 +2,8 @@ import telebot  # pyTelegramBotAPI	4.3.1
 from telebot import types
 import requests
 import bs4
-
+import botGames
+from class_menu import Menu, Users
 bot = telebot.TeleBot('5129343704:AAEQ0O6FhQ_tno-PJzgaLe4cDy0vZHGWa00')
 
 
@@ -13,63 +14,72 @@ def start(message, res=False):
 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("👋 Главное меню")
-    btn2 = types.KeyboardButton("❓ Помощь")
+    btn2 = types.KeyboardButton("❓ Помощь(мне нужна)")
     markup.add(btn1, btn2)
 
     bot.send_message(chat_id,
-                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                     text="Привет, {0.first_name}! Я недоббот и скоро я смогу сделать что-то полезное".format(
                          message.from_user), reply_markup=markup)
+    bot.send_sticker(message.chat.id, "CAACAgIAAxkBAAEEQXRiPK2HXCCsXExpgoeJf4ReHXhQOwAC6B8AAtjY4QABDYxCQyBiSoYjBA")
+
+@bot.message_handler(commands="start")
+def command(message, res=False):
+    chat_id = message.chat.id
+    bot.send_sticker(chat_id, "CAACAgIAAxkBAAIaeWJEeEmCvnsIzz36cM0oHU96QOn7AAJUAANBtVYMarf4xwiNAfojBA")
+    txt_message = f"Привет, {message.from_user.first_name}! Я тестовый бот для курса программирования на языке Python"
+    bot.send_message(chat_id, text=txt_message, reply_markup=Menu.getMenu(chat_id, "Главное меню").markup)
+
+
+@bot.message_handler(content_types=['sticker'])
+def get_messages(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Это " + message.content_type)
+
+    sticker = message.sticker
+    bot.send_message(message.chat.id, sticker)
+
+def goto_menu(chat_id, name_menu):
+    # получение нужного элемента меню
+    cur_menu = Menu.getCurMenu(chat_id)
+    if name_menu == "Выход" and cur_menu != None and cur_menu.parent != None:
+        target_menu = Menu.getMenu(chat_id, cur_menu.parent.name)
+    else:
+        target_menu = Menu.getMenu(chat_id, name_menu)
+
+    if target_menu != None:
+        bot.send_message(chat_id, text=target_menu.name, reply_markup=target_menu.markup)
+
+        # Проверим, нет ли обработчика для самого меню. Если есть - выполним нужные команды
+        if target_menu.name == "Игра в 21":
+            game21 = botGames.newGame(chat_id, botGames.Game21(jokers_enabled=True))  # создаём новый экземпляр игры
+            text_game = game21.get_cards(2)  # просим 2 карты в начале игры
+            bot.send_media_group(chat_id, media=getMediaCards(game21))  # получим и отправим изображения карт
+            bot.send_message(chat_id, text=text_game)
+
+        elif target_menu.name == "Камень, ножницы, бумага":
+            gameRSP = botGames.newGame(chat_id, botGames.GameRPS())  # создаём новый экземпляр игры
+            text_game = "<b>Победитель определяется по следующим правилам:</b>\n" \
+                        "1. Камень побеждает ножницы\n" \
+                        "2. Бумага побеждает камень\n" \
+                        "3. Ножницы побеждают бумагу"
+            bot.send_photo(chat_id, photo="https://i.ytimg.com/vi/Gvks8_WLiw0/maxresdefault.jpg", caption=text_game, parse_mode='HTML')
+
+        return True
+    else:
+        return False
+
+#------------------------------------------------------------------------
+
+
+
 
 
 # -----------------------------------------------------------------------
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    chat_id = message.chat.id
-    ms_text = message.text
-
-    if ms_text == "Главное меню" or ms_text == "👋 Главное меню" or ms_text == "Вернуться в главное меню":  # ..........
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Развлечения")
-        btn2 = types.KeyboardButton("WEB-камера")
-        btn3 = types.KeyboardButton("Управление")
-        back = types.KeyboardButton("Помощь")
-        markup.add(btn1, btn2, btn3, back)
-        bot.send_message(chat_id, text="Вы в главном меню", reply_markup=markup)
-
-    elif ms_text == "Развлечения":  # ..................................................................................
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Прислать собаку")
-        btn2 = types.KeyboardButton("Прислать анекдот")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
-        bot.send_message(chat_id, text="Развлечения", reply_markup=markup)
-
-    elif ms_text == "/dog" or ms_text == "Прислать собаку":  # .........................................................
-        contents = requests.get('https://random.dog/woof.json')
-        urlDOG = contents['url']
-        bot.send_photo(chat_id, photo=urlDOG, caption="Лови собачку")
-
-
-    elif ms_text == "Прислать анекдот":  # .............................................................................
-        bot.send_message(chat_id, text=get_anekdot())
-
-    elif ms_text == "WEB-камера":
-        bot.send_message(chat_id, text="еще не готово...")
-
-    elif ms_text == "Управление":  # ...................................................................................
-        bot.send_message(chat_id, text="еще не готово...")
-
-    elif ms_text == "Помощь" or ms_text == "/help":  # .................................................................
-        bot.send_message(chat_id, "Автор: ")
-        key1 = types.InlineKeyboardMarkup()
-        btn1 = types.InlineKeyboardButton(text="Напишите автору", url="@t_o_ria")
-        key1.add(btn1)
-        img = open('Швец Андрей.png', 'rb')
-        bot.send_photo(message.chat.id, img, reply_markup=key1)
-
-    else:  # ...........................................................................................................
-        bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
-
+def getMediaCards(game21):
+    medias = []
+    for url in game21.arr_cards_URL:
+        medias.append(types.InputMediaPhoto(url))
+    return medias
     # ----------------------------------------------------------------------
 
 
